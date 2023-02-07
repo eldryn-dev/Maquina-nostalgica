b4a2f3f6792d9193d440faadc1a6c00cc81391fe
Eldryn Santos <eldryn.dev@gmail.com>
2023-02-07 17:05:27 -0300

    começo do projeto

---
+++ Site/scripts/main-script.js
@@ -0,0 +1,5 @@
+var box = window.document
+
+var video = []
+
+
--- Site/scripts/script.js
+++ Site/scripts/script.js
@@ -1,7 +1,11 @@
 var audio = window.document.getElementById('aud')
 audio.volume = 0.2;
 
-function tocar() {
+function tocar(){
     audio.play()
 }
+
+var bg = window.document.querySelector("body")
+var botao = window.document.querySelector("div#botao-container")
+
 
