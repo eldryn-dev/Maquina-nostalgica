c7b160c35b02c50f8bd5bcddaf0c26000a375990
Eldryn Santos <eldryn.dev@gmail.com>
2023-02-09 15:03:05 -0300

    Criada a página de upload e scripts

--- Site/scripts/main-script.js
+++ Site/scripts/main-script.js
@@ -1,5 +1,35 @@
-var box = window.document
+var galery = []
+var video = window.document.getElementById('video')
 
-var video = []
+var v1 = document.getElementById('v1')
+var v2 = document.getElementById('v2')
+var v3 = document.getElementById('v3')
+var v4 = document.getElementById('v4')
+var v5 = document.getElementById('v5')
 
 
+
+c=0
+function salvar(){
+    if (galery.length < 5) {
+        
+        galery.push(video.value)
+        
+        alert(`Recebemos seu link ${galery[c]}`)
+        
+        c++
+
+    }else {
+        alert(`Sua galeria já está cheia`)
+    }
+    
+}
+
+      /*
+        v1.innerHTML = galery[0]
+        v2.innerHTML = galery[1]
+        v3.innerHTML = galery[2]
+        v4.innerHTML = galery[3]
+        v5.innerHTML = galery[4]
+    */
+   
