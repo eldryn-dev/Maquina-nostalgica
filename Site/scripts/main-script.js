c614bd7c233fbcd8fb07cd31693fee4bc87c7aff
Eldryn Santos <eldryn.dev@gmail.com>
2023-02-13 13:59:36 -0300

    galeria
    A galeria agora é mostrada conforme o usuário envia os links

--- Site/scripts/main-script.js
+++ Site/scripts/main-script.js
@@ -9,29 +9,34 @@ var v4 = document.getElementById('v4')
 var v5 = document.getElementById('v5')
 
 
-
+let z=0  //z é uma variavel de controle para parar as funções 'salvar()' e 'mostrar ()'//
 c=0
 function salvar(){
-    if (galery.length < 5) {
+
+    if (galery.length < 5 && z==0) {
         link = video.value    
         //Função toEmbed//
         
         pontofin = parseInt(link.search(/&/))
 
-        alert(pontofin)
+        //alert(pontofin)//
         
         //cortando a String no &//
         nlink = link.slice(0 , pontofin)
-        alert(nlink)
+        //alert(nlink)//
         
         //Substituindo 'watch?v=' por 'embed/'
         
         linkfin = nlink.replace( "watch?v=" , "embed/")
-        alert(linkfin)
+        //alert(linkfin)//
 
         //fim da função toEmbed//
-
-        galery.push(linkfin)
+        if(galery.length == 0){
+            galery[0] = linkfin
+        }else{
+            galery.push(linkfin)    
+        }
+        
         //tentando armazenar itens entre paginas//
         localStorage.setItem("galeria",galery)
         
@@ -45,7 +50,40 @@ function salvar(){
     
 }
 
-    // Função para mostrar a galeria //
+    
+    /*
+    https://www.youtube.com/watch?v=m05-kE_tSB8&t=383s&ab_channel=JS
+    https://www.youtube.com/watch?v=OpYtndKTYdY&ab_channel=Clark
+    https://www.youtube.com/watch?v=p1IIHbbdekA&ab_channel=beyondwoods
+    https://www.youtube.com/watch?v=lEqFAvYivNk&ab_channel=averrsa
+    https://www.youtube.com/watch?v=1cJTxRNGUCs&ab_channel=loveless
+    */
+// mostrar a galeria //
+
+function mostrar(){ 
+    // var spc vai ser o espaço onde vou mostrar os links//
+    let spc = document.getElementById('gal')
+    // var item vai ser cada link individual//
+    let cont = galery.length
+    
+    let item = document.createElement("li")    
+    if (galery.length < 5 && z == 0){
+        for (let c = 0; c < galery.length; c++){
+            item.innerHTML= galery[c] 
+        }
+        spc.appendChild(item) 
+    }else if (galery.length = 5 && z == 0) {
+        for (let c = 0; c < galery.length; c++){
+            item.innerHTML= galery[c]
+        }
+        spc.appendChild(item)
+        z=1
+    }
+    
+    
+    }
+
+
 
 
 //Função para reproduzir o input do usuario dentro do iframe//
