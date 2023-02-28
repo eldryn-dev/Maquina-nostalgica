52357b57393aeb2b62f7159570b3cd8b7e9edc65
Eldryn Santos <eldryn.dev@gmail.com>
2023-02-28 12:31:53 -0300

    Update main-script.js

--- Site/scripts/main-script.js
+++ Site/scripts/main-script.js
@@ -5,7 +5,6 @@ var toca = window.document.getElementById('yt-tela')
 let z=0  //z é uma variavel de controle para parar as funções 'salvar()' e 'mostrar ()'//
 c=0
 function salvar(){
-
     if (galery.length < 5 && z==0) {
         link = video.value    
         //Função toEmbed//
@@ -24,7 +23,7 @@ function salvar(){
         //alert(linkfin)//
 
         //remove as caracteriscas do iframe//
-        linkfin += "?controls=0&fs=0"
+        //linkfin += "?controls=0&fs=0"//
         
         //fim da função toEmbed//
         if(galery.length == 0){
@@ -65,8 +64,6 @@ function mostrar(){
         spc.appendChild(item)
         z=1
     }
-    
-    
     }
 
 //Função para reproduzir o input do usuario dentro do iframe//
@@ -75,33 +72,12 @@ function rep(){
     alert(galery[0])
     toca.setAttribute('src' , galery[0])
 }
-   
+//Função para aparecer botões//
+
+
+
 //Função para levar a array para a outra página
 
 function take(){
     localStorage.setItem("galeria",JSON.stringify(galery))
 }
-
-//Função para alterar a string com o link do video para uma string com o link embed//
-
-/*
-function toembed() {
-    link = video.value    
-    //Função toEmbed//
-    
-    pontofin = parseInt(link.search(/&/))
-
-    alert(pontofin)
-    
-    //cortando a String no &//
-    nlink = link.slice(0 , pontofin)
-    alert(nlink)
-    
-    //Substituindo 'watch?v=' por 'embed/'
-    
-    linkfin = nlink.replace( "watch?v=" , "embed/")
-    alert(linkfin)
-
-    //fim da função toEmbed//
-}
-*/
